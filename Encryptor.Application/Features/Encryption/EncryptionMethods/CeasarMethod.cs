083cf8049f3933b758907d226645e153e93ffed3
vladg <vlad.grush.99@gmail.com>
2024-01-16 10:56:06 +0200

    implement caesar cipher

--- Encryptor.Application/Features/Encryption/EncryptionMethods/CeasarMethod.cs
+++ Encryptor.Application/Features/Encryption/EncryptionMethods/CeasarMethod.cs
@@ -1,9 +1,30 @@
-﻿namespace Encryptor.Application.Features.Encryption.EncryptionMethods;
+﻿using System.Text;
+
+namespace Encryptor.Application.Features.Encryption.EncryptionMethods;
 
 public class CaesarMethod : IEncryptor
 {
+    private readonly int _key = 3;
     public string Encrypt(string originalText)
     {
-        throw new NotImplementedException();
+        var sb = new StringBuilder();
+
+        foreach (var symbol in originalText)
+        {
+            sb.Append(EncryptChar(symbol));
+        }
+
+        return sb.ToString();
+    }
+
+    private char EncryptChar(char symbol)
+    {
+        if (!char.IsLetter(symbol))
+        {
+            return symbol;
+        }
+        
+        char d = char.IsUpper(symbol) ? 'A' : 'a';  
+        return (char)((((symbol + _key) - d) % 26) + d);  
     }
 }
--- Encryptor.Tests/EncryptorTests/CaesarTests.cs
+++ Encryptor.Tests/EncryptorTests/CaesarTests.cs
@@ -1,10 +1,17 @@
-﻿namespace Encryptor.Tests.EncryptorTests;
+﻿using Encryptor.Application.Features.Encryption.EncryptionMethods;
+
+namespace Encryptor.Tests.EncryptorTests;
 
 public class CaesarTests
 {
     [Fact]
-    public void InitEncryption()
+    public void CheckCaesarCipher()
     {
-        Assert.True(true);
+        IEncryptor caesar = new CaesarMethod();
+
+        var actualResult = caesar.Encrypt("hello caesar");
+        var expectedResult = "khoor fdhvdu";
+        
+        Assert.Equal(expectedResult, actualResult);
     }
 }
