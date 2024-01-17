b5e01de5e895af51dfeb62a63dbf8898f39f21db
vladg <vlad.grush.99@gmail.com>
2024-01-17 18:32:14 +0200

    implement vigenere cipher

--- Encryptor.Application/Features/Encryption/EncryptionMethods/VigenereEncryption.cs
+++ Encryptor.Application/Features/Encryption/EncryptionMethods/VigenereEncryption.cs
@@ -1,9 +1,51 @@
-﻿namespace Encryptor.Application.Features.Encryption.EncryptionMethods;
+﻿using System.Text;
+
+namespace Encryptor.Application.Features.Encryption.EncryptionMethods;
 
 public class VigenereEncryption : IEncryptor
 {
+    private readonly string _secretKey = "secretkey";
     public string Encrypt(string originalText)
     {
-        throw new NotImplementedException();
+        var gamma = GetRepeatKey(originalText.Length);
+        var sbResult = new StringBuilder();
+        var alphabetSize = 26;
+        int gammaIndex = 0;
+        for (int i = 0; i < originalText.Length; i++)
+        {
+            var letter = originalText[i];
+            if (!char.IsLetter(letter))
+            {
+                sbResult.Append(letter);
+            }
+            else
+            {
+                var d = char.IsUpper(letter) ? 'A' : 'a';
+                var letterCode = letter - d;
+                var gammaCode = char.IsUpper(letter) ? 
+                    char.ToUpper(gamma[gammaIndex]) - d
+                    : gamma[gammaIndex] - d;
+                
+                var encryptedLetterCode = (letterCode + gammaCode) % alphabetSize;
+
+                sbResult.Append((char)(encryptedLetterCode + d));
+                gammaIndex++;
+            }
+        }
+
+        return sbResult.ToString();
+    }
+    
+    private string GetRepeatKey(int originalTextLength)
+    {
+        var key = _secretKey;
+        var repeatedKey = new StringBuilder(originalTextLength);
+
+        for (int i = 0; i < originalTextLength; i++)
+        {
+            repeatedKey.Append(key[i % key.Length]);
+        }
+
+        return repeatedKey.ToString();
     }
 }
---
+++ Encryptor.Tests/EncryptorTests/VigenereTests.cs
@@ -0,0 +1,17 @@
+﻿using Encryptor.Application.Features.Encryption.EncryptionMethods;
+
+namespace Encryptor.Tests.EncryptorTests;
+
+public class VigenereTests
+{
+    [Fact]
+    public void CheckVigenereCipher__Test()
+    {
+        IEncryptor vigenereCipher = new VigenereEncryption();
+
+        var actualResult = vigenereCipher.Encrypt("Hello world!");
+        var expectedResult = "Zincs pyvjv!";
+        
+        Assert.Equal(expectedResult, actualResult);
+    }
+}
