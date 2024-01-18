b5cdc4ede963f12e225c9145bf399c5b3a3d8e91
vladg <vlad.grush.99@gmail.com>
2024-01-18 13:58:59 +0200

    tests coverage logging info in xor cipher

--- Encryptor.Application/Features/Encryption/EncryptionMethods/XorEncryption.cs
+++ Encryptor.Application/Features/Encryption/EncryptionMethods/XorEncryption.cs
@@ -1,10 +1,12 @@
 ﻿using System.Text;
+using Encryptor.Application.Features.Logger;
 
 namespace Encryptor.Application.Features.Encryption.EncryptionMethods;
 
-public class XorEncryption : IEncryptor
+public class XorEncryption(IEnumerable<IAppLogger> _loggers) : IEncryptor
 {
     private readonly string _key = "qwerty";
+    
     public string Encrypt(string originalText)
     {
         var currentKey = GetRepeatKey(originalText.Length);
@@ -12,6 +14,11 @@ public class XorEncryption : IEncryptor
         for (var i = 0; i < originalText.Length; i++)
         {
              sbResult.Append(((char)(originalText[i] ^ currentKey[i])));
+        }
+
+        foreach (var logger in _loggers)
+        {
+            logger.Log($"Xor cipher, original message: \'{originalText}\', encrypted text: \'{sbResult}\'");
         }
 
         return sbResult.ToString();
--- Encryptor.Tests/EncryptorTests/XorTests.cs
+++ Encryptor.Tests/EncryptorTests/XorTests.cs
@@ -1,5 +1,6 @@
 ﻿using System.Text;
 using Encryptor.Application.Features.Encryption.EncryptionMethods;
+using Encryptor.Application.Features.Logger;
 
 namespace Encryptor.Tests.EncryptorTests;
 
@@ -8,12 +9,77 @@ public class XorTests
     [Fact]
     public void XorTestEncryptionText__test()
     {
-        IEncryptor xorCipher = new XorEncryption();
+        IEncryptor xorCipher = new XorEncryption(new List<IAppLogger>());
 
         var actualResult = xorCipher.Encrypt("Hello World!");
         var actualHexResult = BitConverter.ToString(Encoding.Default.GetBytes(actualResult)).Replace("-", " ");
         var expectedHexResult = "39 12 09 1E 1B 59 26 18 17 1E 10 58";
         
         Assert.Equal(expectedHexResult, actualHexResult);
+    }
+
+    [Fact]
+    public void CheckXorCipherLoggingToConsole()
+    {
+        var stringWriter = new StringWriter();
+        Console.SetOut(stringWriter);
+        IEncryptor xorCipher = new XorEncryption(new List<IAppLogger>() {new ConsoleLogger()});
+
+        var actualResult = xorCipher.Encrypt("Hello World!");
+        var actualHexResult = BitConverter.ToString(Encoding.Default.GetBytes(actualResult)).Replace("-", " ");
+        var expectedHexResult = "39 12 09 1E 1B 59 26 18 17 1E 10 58";
+        
+        Assert.Equal(expectedHexResult, actualHexResult);
+
+        var actualConsoleOut = stringWriter.ToString().Trim();
+        var expectedConsoleOut = $"LOG: Xor cipher, original message: \'Hello World!\', encrypted text: \'{actualResult}\'";
+        
+        Assert.Equal(expectedConsoleOut, actualConsoleOut);
+    }
+    
+    [Fact]
+    public void CheckXorCipherLoggingToFile()
+    {
+        File.Delete("./Log.txt");
+        IEncryptor xorCipher = new XorEncryption(new List<IAppLogger>() { new FileLogger() });
+
+        var actualResult = xorCipher.Encrypt("Hello World!");
+        var actualHexResult = BitConverter.ToString(Encoding.Default.GetBytes(actualResult)).Replace("-", " ");
+        var expectedHexResult = "39 12 09 1E 1B 59 26 18 17 1E 10 58";
+        
+        Assert.Equal(expectedHexResult, actualHexResult);
+
+        var actualLogLine = File.ReadAllLines("./Log.txt").First();
+        var expectedLogLine =
+            $"Xor cipher, original message: \'Hello World!\', encrypted text: \'{actualResult}'";
+        
+        Assert.Equal(expectedLogLine, actualLogLine);
+    }
+    
+    [Fact]
+    public void CheckXorCipherLoggingToConsoleAndFile()
+    {
+        var stringWriter = new StringWriter();
+        Console.SetOut(stringWriter);
+        File.Delete("./Log.txt");
+        
+        IEncryptor xorCipher = new XorEncryption(new List<IAppLogger>() { new ConsoleLogger(), new FileLogger() });
+
+        var actualResult = xorCipher.Encrypt("Hello World!");
+        var actualHexResult = BitConverter.ToString(Encoding.Default.GetBytes(actualResult)).Replace("-", " ");
+        var expectedHexResult = "39 12 09 1E 1B 59 26 18 17 1E 10 58";
+        
+        Assert.Equal(expectedHexResult, actualHexResult);
+        
+        var actualConsoleOut = stringWriter.ToString().Trim();
+        var expectedConsoleOut = $"LOG: Xor cipher, original message: \'Hello World!\', encrypted text: \'{actualResult}\'";
+        
+        Assert.Equal(expectedConsoleOut, actualConsoleOut);
+        
+        var actualLogLine = File.ReadAllLines("./Log.txt").First();
+        var expectedLogLine =
+            $"Xor cipher, original message: \'Hello World!\', encrypted text: \'{actualResult}'";
+        
+        Assert.Equal(expectedLogLine, actualLogLine);
     }
 }
