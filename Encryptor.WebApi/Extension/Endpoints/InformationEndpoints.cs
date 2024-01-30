c6b93ef606d9569ab7fdb6b7e91f6e48904f8ffc
vladg <vlad.grush.99@gmail.com>
2024-01-30 16:17:53 +0200

    provide new information endpoints

--- Encryptor.WebApi/Extension/Endpoints/InformationEndpoints.cs
+++ Encryptor.WebApi/Extension/Endpoints/InformationEndpoints.cs
@@ -1,4 +1,8 @@
-﻿using Encryptor.Application.AggregateWebApi.Features.Information.GetFullInfo;
+﻿using Encryptor.Application.AggregateWebApi.Features.Information.AmountUsageCiphers;
+using Encryptor.Application.AggregateWebApi.Features.Information.AmountUsageMessages;
+using Encryptor.Application.AggregateWebApi.Features.Information.GetFullInfo;
+using Encryptor.Application.AggregateWebApi.Features.Information.GetMethodUsageInfo;
+using Encryptor.Application.AggregateWebApi.Features.Information.InformationOfAllMessages;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Encryptor.WebApi.Extension.Endpoints;
@@ -8,7 +12,12 @@ public static class InformationEndpoints
     public static RouteGroupBuilder AddInformationEndpoints(this RouteGroupBuilder route)
     {
         route.MapGet("/", ([FromServices]GetFullInfoHandler handler) => handler.Handle());
-        route.MapGet("/{name}", ([FromQuery] string name) => name);
+        route.MapGet("/{name}", 
+            ([FromQuery] string name, GetMethodUsageInfoHandler handler) 
+            => handler.Handle(name));
+        route.MapGet("usage-ciphers", (AmountOfUsageCiphersHandler handler) => handler.Handle());
+        route.MapGet("all-messages", (InformationOfAllMessagesHandler handler) => handler.Handle());
+        route.MapGet("usage-messages", (AmountOfUsageMessagesHandler handler) => handler.Handle());
         return route;
     }
 }
